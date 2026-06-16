2fbbfa2831635cc3cc0a331505db261141f27465
GustaDoPix <gustavodoamaral2015@gmail.com>
2026-06-16 12:57:08 -0300

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,8 @@
+nota1 = int(input("Digite a nota do 1° bimestre: "))
+nota2 = int(input("Digite a nota do 2° bimestre: "))
+nota3 = int(input("Digite a nota do 3° bimestre: "))
+nota4 = int(input("Digite a nota do 4° bimestre: "))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print("A sua nota é: ", media)
