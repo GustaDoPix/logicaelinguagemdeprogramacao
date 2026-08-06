51d151aa5b6898eb6b03f696319f89064674517f
GustaDoPix <gustavodoamaral2015@gmail.com>
2026-08-06 12:05:53 -0300

    Add files via upload

---
+++ completar o código da professora.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite a sua primeira nota: "))
+nota2 = float(input("Digite a sua segunda nota: "))
+nota3 = float(input("Digite a sua terceira nota: "))
+nota4 = float(input("Digite a sua quarta nota: "))
+
+média = (nota1+nota2+nota3+nota4)/4
+
+print("A sua nota final é", média)
