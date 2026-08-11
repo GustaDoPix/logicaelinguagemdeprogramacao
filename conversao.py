bbd62c63db59020a67acd66fc03915d51043f308
GustaDoPix <gustavodoamaral2015@gmail.com>
2026-08-11 13:06:28 -0300

    Add files via upload

---
+++ conversao.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite o primeiro número:"))
+n2 = float(input("Digite o segundo número:"))
+n3 = float(input("Digite o terceiro número:"))
+
+resultado = (n2 * n3)/n1
+
+print(" O resultado é:", resultado)
