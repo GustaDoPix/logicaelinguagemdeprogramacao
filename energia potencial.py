d3283e4905e9086cf8c66a14d8cd5f67372dda7f
GustaDoPix <gustavodoamaral2015@gmail.com>
2026-08-11 12:56:04 -0300

    Add files via upload

---
+++ energia potencial.py
@@ -0,0 +1,7 @@
+massa = float(input("Digite a quantidade de massa: "))
+gravidade = float(input("Digite a gravidade: "))
+altura = float(input("Digite a altura:"))
+
+energia = massa * gravidade * altura
+
+print("A energia total é:", energia)
