63dd6c1bb8d7c67158c900246a4babe3396f0344
GustaDoPix <gustavodoamaral2015@gmail.com>
2026-08-25 13:06:57 -0300

    Add files via upload

---
+++ if e else.py
@@ -0,0 +1,14 @@
+nota1 = float(input("Digite a nota do primeiro bimestre:"))
+nota2 = float(input("Digite a nota do segundo bimestre:"))
+nota3 = float(input("Digite a nota do terceiro bimestre:"))
+nota4 = float(input("Digite a nota do quarto bimestre:"))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+
+if media < 5:
+ print("Voce está REPROVADO")
+
+else:
+  print("Voce está APROVADO")
+print("A sua média é: ", media)6
