cb340f96832e78b42b4ef39bb33cbc891db8a6f0
GustaDoPix <gustavodoamaral2015@gmail.com>
2026-09-03 13:11:59 -0300

    Add files via upload

---
+++ farmexx3.py
@@ -0,0 +1,5 @@
+temperatura = float(input("Digite a temperatura: "))
+if temperatura >= 30:
+   print("Está quente")
+else:
+   print("Temperatura agradável")
---
+++ farmexx4.py
@@ -0,0 +1,7 @@
+senha = input("Digite a senha: ")
+if senha == "GugexxAurudoxx":
+    print("senha correta")
+else:
+    print("senha incorreta")
+
+   
